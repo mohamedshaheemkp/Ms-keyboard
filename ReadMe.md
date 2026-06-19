be0c1aa5ab9e631e02f875d8d92c5257c0f8d708
mohamedshaheemkp <mohamedshaheemkp74@gmail.com>
2026-06-19 16:15:02 +0530

    Portfolio progress before dependency repair

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+This is using nimbus keyboard = https://shaheemskeyboard.prismic.io/builder/working
+yt = https://www.youtube.com/watch?v=R7l4uVMWRF0&list=PL6sMgbfvAyynQug_WkQHsJG9iWoRYM9xG&index=5&t=23460s
+git = 
+documentation = https://prismic.io/courses/nimbus-keyboards
+youtube = https://www.youtube.com/watch?v=R7l4uVMWRF0&list=PL6sMgbfvAyynQug_WkQHsJG9iWoRYM9xG&index=5&t=23460s
--- ms-keyboard/slices/Hero/index.tsx
+++ ms-keyboard/slices/Hero/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { Span } from "next/dist/trace";
 
 /**
  * Props for `Hero`.
@@ -15,14 +16,25 @@ const Hero: FC<HeroProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="text-white relative h-dvh text-shadow-black/30 text-shadow-lg "
     >
-      Placeholder component for hero (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use your own AI agent with the Prismic CLI
-       * 📚 Docs: https://prismic.io/docs/ai#create-slices
-       */}
+      <div className="hero-scene sticky pointer-events-none top-0 h-dvh w-full">
+
+        {/* canvas goes here */}
+      </div>
+    <div className="hero-content">
+    </div>
+
+
+      
+
+    <PrismicRichText field={slice.primary.heading} />
+    <PrismicRichText field={slice.primary.body} />
+    <button className="font-bold-slanted group flex w-fit cursor-pointer items-center gap-1 rounded bg-[#01A7E1] px-3 py-1
+      text-2x1 uppercase transition disabled:grayscale">
+      {slice.primary.buy_button_text}
+      <span className = "group-hover:translate-x-1 transition">{">"}</span>
+    </button>
     </section>
   );
 };
