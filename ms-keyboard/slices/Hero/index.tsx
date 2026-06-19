6689dd3a41980e4671d174efc7d9ee465682e931
mohamedshaheemkp <mohamedshaheemkp74@gmail.com>
2026-06-19 12:07:04 +0530

    Initial commit 2

---
+++ ms-keyboard/slices/Hero/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      Placeholder component for hero (variation: {slice.variation}) slices.
+      <br />
+      <strong>You can edit this slice directly in your code editor.</strong>
+      {/**
+       * 💡 Use your own AI agent with the Prismic CLI
+       * 📚 Docs: https://prismic.io/docs/ai#create-slices
+       */}
+    </section>
+  );
+};
+
+export default Hero;
